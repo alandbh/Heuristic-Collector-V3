6db91b93a44090c2e52531eacb3074413ca6075f
Alan Vasconcelos <alandbh@gmail.com>
2023-07-14 10:55:06 -0300

    Adding Nubank previous data

---
+++ components/edition2021/nubank.js
@@ -0,0 +1,123 @@
+export const nubank = {
+    gerais: [
+        {
+            id: 1.2,
+            scoreValuePrev: 2,
+            averageScoreValuePrev: 4.71,
+        },
+        {
+            id: 1.3,
+            scoreValuePrev: 1,
+            averageScoreValuePrev: 1.38,
+        },
+        {
+            id: 6.1,
+            scoreValuePrev: 5,
+            averageScoreValuePrev: 4.04,
+        },
+        {
+            id: 7.2,
+            scoreValuePrev: 5,
+            averageScoreValuePrev: 2.92,
+        },
+        {
+            id: 7.4,
+            scoreValuePrev: 4,
+            averageScoreValuePrev: 3.42,
+        },
+        {
+            id: 7.5,
+            scoreValuePrev: 5,
+            averageScoreValuePrev: 4,
+        },
+        {
+            id: 10.1,
+            scoreValuePrev: 5,
+            averageScoreValuePrev: 3.75,
+        },
+        {
+            id: 11.1,
+            scoreValuePrev: 5,
+            averageScoreValuePrev: 4.71,
+        },
+        {
+            id: 15.1,
+            scoreValuePrev: 2,
+            averageScoreValuePrev: 1.46,
+        },
+        {
+            id: 15.2,
+            scoreValuePrev: 1,
+            averageScoreValuePrev: 2.5,
+        },
+        {
+            id: 15.3,
+            scoreValuePrev: 1,
+            averageScoreValuePrev: 3.58,
+        },
+    ],
+    abertura: [
+        {
+            id: 3.1,
+            scoreValuePrev: 5,
+            averageScoreValuePrev: 4.08,
+        },
+        {
+            id: 6.1,
+            scoreValuePrev: 5,
+            averageScoreValuePrev: 4.04,
+        },
+        {
+            id: 7.1,
+            scoreValuePrev: 5,
+            averageScoreValuePrev: 4.88,
+        },
+        {
+            id: 8.1,
+            scoreValuePrev: 5,
+            averageScoreValuePrev: 4.17,
+        },
+        {
+            id: 8.2,
+            scoreValuePrev: 5,
+            averageScoreValuePrev: 4.17,
+        },
+        {
+            id: 10.1,
+            scoreValuePrev: 5,
+            averageScoreValuePrev: 4.67,
+        },
+        {
+            id: 12.1,
+            scoreValuePrev: 1,
+            averageScoreValuePrev: 2.83,
+        },
+    ],
+    cartao: [
+        {
+            id: 4.2,
+            scoreValuePrev: 5,
+            averageScoreValuePrev: 3.8,
+        },
+        {
+            id: 4.3,
+            scoreValuePrev: 5,
+            averageScoreValuePrev: 5,
+        },
+        {
+            id: 6.1,
+            scoreValuePrev: 5,
+            averageScoreValuePrev: 3.3,
+        },
+        {
+            id: 7.1,
+            scoreValuePrev: 5,
+            averageScoreValuePrev: 4,
+        },
+        {
+            id: 14.1,
+            scoreValuePrev: 1,
+            averageScoreValuePrev: 3.1,
+        },
+    ],
+};
--- components/edition2021/prevScores.js
+++ components/edition2021/prevScores.js
@@ -1,8 +1,10 @@
 // test
 import { bancoDoBrasil } from "./banco-do-brasil";
 import { btg } from "./btg";
+import { nubank } from "./nubank";
 
 export const prevScores = {
     "banco-do-brasil": bancoDoBrasil,
     btg,
+    nubank,
 };
