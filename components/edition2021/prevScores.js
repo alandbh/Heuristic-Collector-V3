26bfffc71afed7fc2c2617b3975be6374aec37cc
Alan Vasconcelos <alandbh@gmail.com>
2023-07-17 21:00:10 -0300

    Adding Bradesco

--- components/edition2021/prevScores.js
+++ components/edition2021/prevScores.js
@@ -2,9 +2,11 @@
 import { bancoDoBrasil } from "./banco-do-brasil";
 import { btg } from "./btg";
 import { nubank } from "./nubank";
+import { bradesco } from "./bradesco";
 
 export const prevScores = {
     "banco-do-brasil": bancoDoBrasil,
     btg,
     nubank,
+    bradesco,
 };
