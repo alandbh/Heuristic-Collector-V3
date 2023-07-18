8c257f47c17723a4d20e00f474b7aad0145fa9f4
Alan Vasconcelos <alandbh@gmail.com>
2023-07-18 15:15:35 -0300

    Banco Original e Picpay previous scores

--- components/edition2021/prevScores.js
+++ components/edition2021/prevScores.js
@@ -3,10 +3,14 @@ import { bancoDoBrasil } from "./banco-do-brasil";
 import { btg } from "./btg";
 import { nubank } from "./nubank";
 import { bradesco } from "./bradesco";
+import { picpay } from "./picpay";
+import { bancoOriginal } from "./banco-original";
 
 export const prevScores = {
     "banco-do-brasil": bancoDoBrasil,
     btg,
     nubank,
     bradesco,
+    picpay,
+    bancoOriginal,
 };
