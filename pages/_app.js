90686dc216b7ed60494316af8e3e7bc2beb816fd
Alan Vasconcelos <alandbh@gmail.com>
2024-09-10 23:33:01 -0300

    WIP: Adding event listener in sw.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,21 @@ if (typeof window !== "undefined") {
     } else {
         document.documentElement.classList.remove("dark");
     }
+
+    window.addEventListener("load", async () => {
+        if (location.search.includes("api/upload")) {
+            const keys = await caches.keys();
+            const mediaCache = await caches.open(
+                keys.filter((key) => key.startsWith("media"))[0]
+            );
+            const image = await mediaCache.match("shared-image");
+            if (image) {
+                const blob = await image.blob();
+                await mediaCache.delete("shared-image");
+                // Handle the shared file somehow.
+            }
+        }
+    });
 }
 function MyApp({ Component, pageProps }) {
     return (
