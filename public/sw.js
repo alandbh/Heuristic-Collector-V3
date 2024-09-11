8e40728aca4c38813ed97c51d2d4da96db6d3dee
Alan Vasconcelos <alandbh@gmail.com>
2024-09-11 00:00:12 -0300

    WIP: trying to move the event listener to the end of sw.js

--- public/sw.js
+++ public/sw.js
@@ -67,22 +67,6 @@ if (!self.define) {
             return exports;
         });
     };
-
-    self.addEventListener("fetch", (event) => {
-        if (event.request.method !== "POST") {
-            event.respondWith(fetch(event.request));
-            return;
-        }
-
-        const formDataPromise = event.request.formData();
-        event.respondWith(
-            formDataPromise.then((formData) => {
-                const link = formData.get("link") || "";
-                saveBookmark(link);
-                return new Response(`Bookmark saved: ${link}`);
-            })
-        );
-    });
 }
 define(["./workbox-eac1af49"], function (workbox) {
     "use strict";
@@ -137,5 +121,24 @@ define(["./workbox-eac1af49"], function (workbox) {
         }),
         "GET"
     );
+});
+self.addEventListener("fetch", (fetchEvent) => {
+    if (
+        fetchEvent.request.url.endsWith("/api/upload/") &&
+        fetchEvent.request.method === "POST"
+    ) {
+        return fetchEvent.respondWith(
+            (async () => {
+                const formData = await fetchEvent.request.formData();
+                const image = formData.get("image");
+                const keys = await caches.keys();
+                const mediaCache = await caches.open(
+                    keys.filter((key) => key.startsWith("media"))[0]
+                );
+                await mediaCache.put("shared-image", new Response(image));
+                return Response.redirect("./?share-target", 303);
+            })()
+        );
+    }
 });
 //# sourceMappingURL=sw.js.map
