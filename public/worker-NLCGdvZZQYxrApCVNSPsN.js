37bf7f33892d58b8e77eee84ef279d56321c3f44
Alan Vasconcelos <alandbh@gmail.com>
2024-09-25 13:10:57 -0300

    Removing the Share Target API from both Manifest and Custom Worker

--- public/worker-NLCGdvZZQYxrApCVNSPsN.js
+++
@@ -1,1 +0,0 @@
-self.addEventListener("fetch",(e=>{"POST"===e.request.method?e.respondWith((async()=>{const t=(await e.request.formData()).get("link")||"",r=await saveBookmark(t);return Response.redirect(r,303)})()):e.respondWith(fetch(e.request))}));
