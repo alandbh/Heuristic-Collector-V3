60cdf455fe90596efc340a0dfdf2c71bdb5fab19
Alan Vasconcelos <alandbh@gmail.com>
2025-07-26 01:17:01 -0300

    Refactor Toggle component layout and simplify structure

--- components/Toggle/index.jsx
+++ components/Toggle/index.jsx
@@ -25,36 +25,32 @@ function Toggle({ onChange, selected, disable = false }) {
 
     return (
         <>
-            <div className="h-6 overflow-x-scroll w-full lg:overflow-x-visible  rounded-full flex items-center switch-scroll">
+            <div
+                className={`wrapper flex  w-fit border  dark:border-white/60 h-[34px] rounded-full relative ${
+                    option ? "bg-green-300" : "bg-transparent border-blue-300"
+                }`}
+            >
                 <div
-                    className={`wrapper flex  w-fit border  dark:border-white/60 h-[34px] rounded-full relative ${
-                        option
-                            ? "bg-green-300"
-                            : "bg-transparent border-blue-300"
-                    }`}
+                    className={`bubble z-0 transition duration-300 rounded-full top-[2px] w-[28px] h-[28px] border  ${
+                        option ? "bg-white" : "bg-blue-100 border-blue-500"
+                    } bg-blue-100 absolute ${getBubblePosition()}`}
+                ></div>
+                <div
+                    className={`container flex w-[60px] justify-between z-10 dark:text-white/60`}
                 >
-                    <div
-                        className={`bubble z-0 transition duration-300 rounded-full top-[2px] w-[28px] h-[28px] border  ${
-                            option ? "bg-white" : "bg-blue-100 border-blue-500"
-                        } bg-blue-100 absolute ${getBubblePosition()}`}
-                    ></div>
-                    <div
-                        className={`container flex w-[60px] justify-between z-10 dark:text-white/60`}
+                    <label
+                        style={{ flex: "0 0 60px" }}
+                        className="flex w-[44px] h-[34px] items-center justify-center cursor-pointer"
                     >
-                        <label
-                            style={{ flex: "0 0 44px" }}
-                            className="flex w-[44px] h-[44px] items-center justify-center cursor-pointer"
-                        >
-                            <input
-                                type="checkbox"
-                                className="sr-only"
-                                name="worktype"
-                                value={option}
-                                onChange={(ev) => handleOnChange(ev)}
-                                disabled={disable}
-                            />
-                        </label>
-                    </div>
+                        <input
+                            type="checkbox"
+                            className="sr-only"
+                            name="worktype"
+                            value={option}
+                            onChange={(ev) => handleOnChange(ev)}
+                            disabled={disable}
+                        />
+                    </label>
                 </div>
             </div>
         </>
