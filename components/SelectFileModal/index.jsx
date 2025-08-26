3f4a5a8bd1720cf4ae6fb9281af3d1e882236bb6
Alan Vasconcelos <alandbh@gmail.com>
2025-08-26 01:44:05 -0300

    Refactor SelectFileModal to improve file fetching logic and UI interactions

--- components/SelectFileModal/index.jsx
+++ components/SelectFileModal/index.jsx
@@ -8,14 +8,12 @@ export default function SelectFileModal({
     currentJourney,
     isOpen,
     onClose,
-    onCancel,
-    onInsert,
     // files,
     selectedFiles = [],
     onSelectionChange,
 }) {
     const [files, setFiles] = useState([]);
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const [errorMessage, setErrorMessage] = useState(null);
 
     useEffect(() => {
@@ -34,41 +32,43 @@ export default function SelectFileModal({
     useEffect(() => {
         if (!isOpen) return;
         console.log("ffffff OPEN");
+        if (files.length > 0) return;
+        fetchFiles();
 
         // Fetch files from the API
-        async function fetchFiles() {
-            // https://heuristic-v4.vercel.app/api/listfolders?folderid=1JKf3bzWGCz27Jr4VBnJc94tr41o9QbwN
-            console.log("ffffff FETCH");
-            try {
-                console.log("ffffff TRY");
-                const response = await fetch(
-                    `https://heuristic-v4.vercel.app/api/listfolders?folderid=${evidenceFolderId}`,
-                    {
-                        headers: {
-                            "Content-Type": "application/json",
-                            api_key: "20rga25",
-                        },
-                        method: "GET",
-                    }
-                );
-                const driveData = await response.json();
-                const evidenceFiles = getEvidenceFiles(
-                    driveData,
-                    currentPlayer,
-                    currentJourney
-                );
-                // console.log("Fetched files:", evidenceFiles);
-                setFiles(evidenceFiles || []);
-                setLoading(false);
-            } catch (error) {
-                console.error("Error fetching files:", error);
-                setFiles([]);
-                setErrorMessage("Failed to load files.");
-            }
-        }
-
-        fetchFiles();
     }, [isOpen]);
+
+    async function fetchFiles() {
+        setLoading(true);
+        // https://heuristic-v4.vercel.app/api/listfolders?folderid=1JKf3bzWGCz27Jr4VBnJc94tr41o9QbwN
+        console.log("ffffff FETCH");
+        try {
+            console.log("ffffff TRY");
+            const response = await fetch(
+                `https://heuristic-v4.vercel.app/api/listfolders?folderid=${evidenceFolderId}`,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        api_key: "20rga25",
+                    },
+                    method: "GET",
+                }
+            );
+            const driveData = await response.json();
+            const evidenceFiles = getEvidenceFiles(
+                driveData,
+                currentPlayer,
+                currentJourney
+            );
+            // console.log("Fetched files:", evidenceFiles);
+            setFiles(evidenceFiles || []);
+            setLoading(false);
+        } catch (error) {
+            console.error("Error fetching files:", error);
+            setFiles([]);
+            setErrorMessage("Failed to load files.");
+        }
+    }
 
     const handleCheckboxChange = (e, fileObj) => {
         if (e.target.checked) {
@@ -92,10 +92,10 @@ export default function SelectFileModal({
                         Select the files
                     </h2>
                     <button
-                        onClick={onClose}
+                        onClick={() => fetchFiles()}
                         className="text-slate-500/70 px-5 py-1 rounded-md hover:text-gray-800 border border-slate-400 hover:bg-blue-100"
                     >
-                        <span>Close</span>
+                        <span>Refresh</span>
                     </button>
                 </div>
 
@@ -139,18 +139,12 @@ export default function SelectFileModal({
                 )}
 
                 {/* Footer */}
-                <div className="__flex justify-end border-t border-gray-200 pt-4 hidden">
+                <div className="flex justify-end border-t border-gray-200 pt-4">
                     <button
-                        onClick={onCancel}
-                        className=" text-blue-500 font-bold py-2 px-4 rounded hover:bg-blue-700 transition-colors"
-                    >
-                        Cancel
-                    </button>
-                    <button
-                        onClick={onInsert}
+                        onClick={onClose}
                         className="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700 transition-colors"
                     >
-                        Insert
+                        Close
                     </button>
                 </div>
             </div>
