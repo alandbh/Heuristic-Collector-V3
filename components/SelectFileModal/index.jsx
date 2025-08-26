565a7b0d8988cff0fd4e34188b36bf3aa8b5ae3b
Alan Vasconcelos <alandbh@gmail.com>
2025-08-26 20:17:27 -0300

    Update checkbox styles, add Image and Video icons

--- components/SelectFileModal/index.jsx
+++ components/SelectFileModal/index.jsx
@@ -167,27 +167,31 @@ export default function SelectFileModal({
                     <div className="max-h-[400px] overflow-y-auto flex flex-col gap-2 pr-2 mb-4">
                         {files.map((file) => (
                             <div
-                                className="flex items-center justify-between gap-3 p-2 rounded-md hover:bg-gray-100"
+                                className="flex items-center justify-between gap-3 py-2 px-3 rounded hover:bg-gray-100 [&:has(input:checked)]:bg-blue-100
+        [&:has(input:checked)]:border-blue-500"
                                 key={file.id}
                             >
                                 <label
                                     htmlFor={file.id}
-                                    className="flex items-center gap-3 cursor-pointer"
+                                    className="flex items-center cursor-pointer flex-1 "
                                 >
                                     <input
                                         type="checkbox"
                                         id={file.id}
-                                        // checked={selectedFiles.includes(file.name)}
                                         checked={selectedFiles.some(
                                             (item) => item.id === file.id
                                         )}
                                         onChange={(e) =>
                                             handleCheckboxChange(e, file)
                                         }
-                                        className="h-5 w-5 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
+                                        className="invisible h-0 w-0 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
                                     />
-                                    <span className="text-gray-700">
-                                        {file.type === "video" ? "📹" : "🖼️"}{" "}
+                                    <span className="text-gray-700 flex items-center gap-2 text-ellipsis text-sm">
+                                        {file.type === "video" ? (
+                                            <VideoIcon />
+                                        ) : (
+                                            <ImageIcon />
+                                        )}{" "}
                                         {file.name}
                                     </span>
                                 </label>
@@ -216,6 +220,32 @@ export default function SelectFileModal({
         </div>
     );
 }
+
+const ImageIcon = (props) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        style={{ width: "16px", height: "16px" }}
+        viewBox="0 0 16 16"
+        fill="red"
+    >
+        <path
+            fillRule="evenodd"
+            d="M16 14.222V1.778C16 .796 15.204 0 14.222 0H1.778C.796 0 0 .796 0 1.778v12.444C0 15.204.796 16 1.778 16h12.444c.982 0 1.778-.796 1.778-1.778M4.889 9.333l2.222 2.671L10.222 8l4 5.333H1.778l3.11-4z"
+            clipRule="evenodd"
+        ></path>
+    </svg>
+);
+
+const VideoIcon = (props) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        style={{ width: "16px", height: "16px" }}
+        viewBox="0 0 16 12"
+        fill="#3b82f6"
+    >
+        <path d="m12.8 0 1.6 3.2H12L10.4 0H8.8l1.6 3.2H8L6.4 0H4.8l1.6 3.2H4L2.4 0h-.8C.72 0 .008.72.008 1.6L0 11.2c0 .88.72 1.6 1.6 1.6h12.8c.88 0 1.6-.72 1.6-1.6V0z"></path>
+    </svg>
+);
 
 function getEvidenceFiles(driveData, currentPlayer, currentJourney) {
     const playerFolder = driveData.find((p) => p.name.trim() === currentPlayer);
